48860d0a7a3c4a4f49eeed2f59ce9553153f4d05
coderun13 <snehalsingh.0407@gmail.com>
2024-04-16 20:50:50 +0530

    data inserting

--- Mongosh/index.js
+++ Mongosh/index.js
@@ -10,7 +10,7 @@ main()
 async function main() {
   await mongoose.connect('mongodb://127.0.0.1:27017/test');
 
-  // use `await mongoose.connect('mongodb://user:password@127.0.0.1:27017/test');` if database has auth enabled
+  // use `await mongoose.connect('mongodb://user:password@127.0.0.1:27017/test');`
 }
 
 const userSchema = new mongoose.Schema({
@@ -18,3 +18,33 @@ const userSchema = new mongoose.Schema({
     email: String,
     age: Number,
 });
+
+const User = mongoose.model("User",userSchema);
+
+const user1 = new User({
+    name: "adam",
+    email: "adam123@gmail.com",
+    age: 34,
+});
+
+user1.save();
+
+const user2 = new User({
+    name: "bob",
+    email: "bob123@gmail.com",
+    age: 20,
+});
+
+user2
+    .save()
+    .then((res)=> {
+        console.log(res);
+    })
+    .catch((err)=>{
+        console.log(err);
+    });
+
+
+
+
+    
