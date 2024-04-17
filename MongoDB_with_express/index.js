c585d2824546bb1a34d17ea5fe6a2dada28c743e
coderun13 <snehalsingh.0407@gmail.com>
2024-04-17 14:36:13 +0530

    new data entered

--- MongoDB_with_express/index.js
+++ MongoDB_with_express/index.js
@@ -2,6 +2,7 @@ const express = require("express"); //require express
 const app = express(); //creating app
 const mongoose = require("mongoose"); //reqiure mongoose
 const path = require("path"); //require path
+const Chat = require("./Models/chat.js");
 
 
 app.set("view engine", "ejs"); //path define
@@ -17,7 +18,18 @@ async function main() {
     await mongoose.connect("mongodb://127.0.0.1:27017/chatup"); //require mongoose
   }
 
+let chat1 = new Chat({
+    from:"snehal",
+    to: "aryan",
+    msg: "Hello how are you",
+    created_at: new Date()
+});
   
+chat1.save().then((res)=> {
+    console.log(res);
+});
+
+
 app.get("/",(req,res) => {
     res.send("root is working"); //connection setup
 });
