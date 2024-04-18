a5379fcf3f9551ec822544c640629da062d4b91a
coderun13 <snehalsingh.0407@gmail.com>
2024-04-19 00:54:24 +0530

    models

---
+++ Mongodb_Relationships/Relationships/models/user.js
@@ -0,0 +1,41 @@
+const mongoose = require("mongoose"); //reqiure mongoose
+const {Schema} = mongoose;
+
+main().then(()=>{
+    console.log("connection successful"); //connection setup
+    })
+    .catch((err) => console.log(err));
+
+async function main() {
+    await mongoose.connect("mongodb://127.0.0.1:27017/relationdemo"); //require mongoose
+  }
+
+  
+  let userSchema= new Schema({
+    username: String,
+    addresses: [
+        {
+            _id: false,
+            location: String,
+            city: String,
+        },
+    ],
+});
+
+const User = mongoose.model("User", userSchema);
+
+const addUsers = async() => {
+    let user1 = new User({
+        username: "sherlockholmes",
+        addresses: [{
+           location:"221B Baker Street", 
+            city:"London"
+        }]
+    });
+
+    user1.addresses.push({location: "P32 WallStreet", city: "London"});
+    let result = await user1.save();
+    console.log(result);
+}
+
+addUsers();
