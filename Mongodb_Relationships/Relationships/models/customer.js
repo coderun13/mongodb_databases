b0ad8ac195e9a8ee2fa77815fc24bdd3ada90751
coderun13 <snehalsingh.0407@gmail.com>
2024-04-30 14:09:12 +0530

    define order schema

--- Mongodb_Relationships/Relationships/models/customer.js
+++ Mongodb_Relationships/Relationships/models/customer.js
@@ -9,4 +9,25 @@ async function main() {
     await mongoose.connect("mongodb://127.0.0.1:27017/relationdemo"); //require mongoose
   }
 
+  //order Schema
+  const orderSchema = new Schema({
+    item: String,
+    price: Number,
+  });
+
+  const Order = mongoose.model("Order", orderSchema);
+
+  //adding data
+  const addOrders = async () => {
+   let res =  await Order.insertMany(
+    [
+        { item:"samosa", price: 12},
+        { item:"chips",price:10},
+        {item:"chocolate", price:40}
+    ]);
+        console.log(res);
+  };
+
+  addOrders();
+
 
